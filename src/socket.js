ce87f934154d824c1f7064af350e251fee4f2376
MozartCuidadorDigital <zorrillanico200312@gmail.com>
2025-09-19 23:18:52 -0500

    ruta ec2

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // ⚠️ Asegúrate de usar la URL de tu backend
-const socket = io("http://localhost:4000", {
+const socket = io("http://52.23.197.194/", {
   transports: ["websocket"],
 });
 
