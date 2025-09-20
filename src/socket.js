321ac778c6d04171d4a4262d386b9f510ccc1f3f
MozartCuidadorDigital <zorrillanico200312@gmail.com>
2025-09-20 16:02:20 -0500

    jaja

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // ⚠️ Asegúrate de usar la URL de tu backend
-const socket = io("https://trivia-backend-omega.vercel.app/", {
+const socket = io("http://52.23.197.194/", {
   transports: ["websocket"],
 });
 
